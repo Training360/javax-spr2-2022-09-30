6454262ca0b9db4a09fa099248e13c6d431e8ac2
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-30 15:10:40 +0200

    REST hívás

---
+++ employees-webclient-client/src/main/java/training/employeeswebclientclient/Employee.java
@@ -0,0 +1,9 @@
+package training.employeeswebclientclient;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private String name;
+}
--- employees-webclient-client/src/main/java/training/employeeswebclientclient/EmployeesWebclientClientApplication.java
+++ employees-webclient-client/src/main/java/training/employeeswebclientclient/EmployeesWebclientClientApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @Slf4j
@@ -16,5 +17,22 @@ public class EmployeesWebclientClientApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Starting application");
+		WebClient client = WebClient.create("http://localhost:8080/api/employees");
+
+//		var json = client
+//				.get()
+//				.exchangeToMono(response -> response.bodyToMono(String.class))
+//				.block();
+//		System.out.println(json);
+
+		WebClient.create("http://localhost:8080/api/employees")
+				.get()
+				.exchangeToFlux(response -> response.bodyToFlux(Employee.class))
+				.toStream().forEach(e -> System.out.println(e.getName()));
+
+//		var i = (Function<String, String>) a -> a;
+
+
+//		System.out.println(employees.getName());
 	}
 }

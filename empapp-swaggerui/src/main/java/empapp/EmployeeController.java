d6b9afe4c0b3ffc8422fc05ebf47c2578f1a93d1
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-30 13:32:29 +0200

    Annotációk az operationön

--- empapp-swaggerui/src/main/java/empapp/EmployeeController.java
+++ empapp-swaggerui/src/main/java/empapp/EmployeeController.java
@@ -3,6 +3,9 @@ package empapp;
 import empapp.dto.CreateEmployeeCommand;
 import empapp.dto.EmployeeDto;
 import empapp.dto.UpdateEmployeeCommand;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/employees")
 @AllArgsConstructor
+@Tag(name = "employees", description = "operations with employees")
 public class EmployeeController {
 
     private EmployeeService employeeService;
@@ -24,6 +28,10 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find employee by id",
+        description = "Find employee by id with addresses.")
+    @ApiResponse(responseCode = "200", description = "employee found")
+    @ApiResponse(responseCode = "404", description = "employee not found")
     public EmployeeDto findEmployeeById(@PathVariable("id") long id) {
         return employeeService.findEmployeeById(id);
     }

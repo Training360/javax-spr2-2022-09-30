a5ba9c7a87f26c717210167078d8e1f5cfbc8e71
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-30 13:28:35 +0200

    DTO-kban annotációk

--- empapp-swaggerui/src/main/java/empapp/dto/CreateEmployeeCommand.java
+++ empapp-swaggerui/src/main/java/empapp/dto/CreateEmployeeCommand.java
@@ -1,6 +1,6 @@
 package empapp.dto;
 
-import empapp.entity.Address;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.List;
@@ -8,7 +8,9 @@ import java.util.List;
 @Data
 public class CreateEmployeeCommand {
 
+    @Schema(description = "the name of the created employee", example = "John Doe")
     private String name;
 
-    private List<Address> addresses;
+
+    private List<AddressDto> addresses;
 }

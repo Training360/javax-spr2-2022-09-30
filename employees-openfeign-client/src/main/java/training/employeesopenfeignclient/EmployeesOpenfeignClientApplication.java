fe90840cced6555d3d7a61f664216707b2139878
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-30 15:12:53 +0200

    OpenFeign client kezdeti projekt

---
+++ employees-openfeign-client/src/main/java/training/employeesopenfeignclient/EmployeesOpenfeignClientApplication.java
@@ -0,0 +1,13 @@
+package training.employeesopenfeignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesOpenfeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesOpenfeignClientApplication.class, args);
+	}
+
+}
---
+++ employees-openfeign-client/src/test/java/training/employeesopenfeignclient/EmployeesOpenfeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package training.employeesopenfeignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesOpenfeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

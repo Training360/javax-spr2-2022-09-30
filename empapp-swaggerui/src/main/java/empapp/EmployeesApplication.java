7d01cdea82308a8705124a107441c5367d6dc4ef
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-30 13:28:23 +0200

    Globális leírás

--- empapp-swaggerui/src/main/java/empapp/EmployeesApplication.java
+++ empapp-swaggerui/src/main/java/empapp/EmployeesApplication.java
@@ -1,7 +1,10 @@
 package empapp;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -10,6 +13,16 @@ public class EmployeesApplication
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI openApi() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Employees application")
+						.description("This application handles the employees.")
+						.version("1.0.0")
+				);
 	}
 
 }
